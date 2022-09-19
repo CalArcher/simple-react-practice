f599ae3d5f2ce0c354cce64fb32d8a44667a86ce
CalArcher <p.calarch@gmail.com>
2022-09-18 23:01:21 -0600

    append api data

---
+++ append-api-data/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+import 'style.css'
+
+const link = 'https://example.gcommer.com/users'
+
+//original link, https://official-joke-api.appspot.com/random_joke, stopped letting me fetch until the next day
+
+const GetNewJoke = ({ fetchMe }) => {
+  const [randomJoke, setRandomJoke] = useState('')
+
+  const fetchJokes = (url) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setRandomJoke(data)
+      })
+  }
+
+  useEffect(() => {
+    console.log('randomJoke: ', randomJoke)
+    fetchJokes(link)
+  }, [fetchMe])
+
+  if (randomJoke) {
+    return <>{JSON.stringify(randomJoke)}</>
+  }
+}
+
+export default function App() {
+  const [fetchMe, setFetchMe] = useState('')
+
+  return (
+    <div id="jokeBox">
+      <button
+        onClick={() => {
+          setFetchMe(fetchMe + 1)
+        }}
+      >
+        Fetch Again
+      </button>
+      <GetNewJoke fetchMe={fetchMe}></GetNewJoke>
+    </div>
+  )
+}
---
+++ append-api-data/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader')
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
