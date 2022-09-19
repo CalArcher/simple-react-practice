1c4a0ea57a23fa8ca35cd35ae53baa0b2fa6328e
CalArcher <p.calarch@gmail.com>
2022-09-19 11:28:49 -0600

    append works

--- append-api-data/src/App.js
+++ append-api-data/src/App.js
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import 'style.css'
 
+const jokeCardStyle = {
+  border: '1px solid red'
+}
+
 const link = 'https://official-joke-api.appspot.com/random_joke'
 
-//original link, https://official-joke-api.appspot.com/random_joke, stopped letting me fetch until the next day
-
 const GetNewJoke = ({ appendJoke }) => {
-  const [randomJoke, setRandomJoke] = useState('')
-  const [allJokes, setAllJokes] = useState([])
+  const [randomJoke, setRandomJoke] = useState([])
+  // const [allJokes, setAllJokes] = useState([])
 
   const fetchJokes = (url) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        setRandomJoke(data)
+        setRandomJoke([...randomJoke, data])
       })
   }
 
@@ -22,15 +24,17 @@ const GetNewJoke = ({ appendJoke }) => {
     fetchJokes(link)
   }, [appendJoke])
 
-  const jokeCardStyle = {
-    border: '1px solid red'
-  }
-
   if (randomJoke) {
     return (
-      <div style={jokeCardStyle}>
-        <span>{randomJoke.setup}</span>
-        <span>{randomJoke.punchline}</span>
+      <div>
+        {randomJoke.map((joke, idx) => {
+          return (
+            <div key={idx} style={jokeCardStyle}>
+              <span>{joke.setup}</span>
+              <span>{joke.punchline}</span>
+            </div>
+          )
+        })}
       </div>
     )
   }
