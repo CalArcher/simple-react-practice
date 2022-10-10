a03c3eda539041aadc2a54b356a224518cad895b
CalArcher <p.calarch@gmail.com>
2022-10-10 17:02:56 -0600

    clean up code

--- green-square-sequence/src/App.js
+++ green-square-sequence/src/App.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
 import 'style.css'
 
+//set how many green squares you want displayed here
 let numOfSquares = 6
 
 export default function App() {
   let [order, setOrder] = useState([])
   let squares = [...Array(numOfSquares)]
 
-  const addEl = (num) => {
+  const addElement = (num) => {
     setOrder((order = order.concat(num)))
   }
 
-  const revSeq = async () => {
+  const reverseSequence = async () => {
     for (let i = 1; i <= numOfSquares; i++) {
       await delay(500)
       const newOrder = order.slice(0, order.length - i)
@@ -24,7 +25,7 @@ export default function App() {
   }
 
   if (order.length >= numOfSquares) {
-    revSeq()
+    reverseSequence()
   }
 
   return squares.map((el, i) => {
@@ -37,7 +38,7 @@ export default function App() {
         className={classes}
         key={i}
         onClick={() => {
-          addEl(i)
+          addElement(i)
         }}
       ></div>
     )
